31723b3a7e2123751dd5c93f0286545a338a92fc
Till Sauerwein <till.sauerwein@web.de>
2022-02-14 09:52:35 +0100

    Update gccalc.py

--- gccalc.py
+++ gccalc.py
@@ -1,6 +1,7 @@
 def calc_gc_content(seq):
-    if len(seq) == 0:
-        return 0.0
-    gc_count = (seq.upper().count("G") + seq.upper().count("C")) * 100
-    gc_value = gc_count / len(seq)
-    return gc_value
+    #if len(seq) == 0:
+    #    return 0.0
+    #gc_count = (seq.upper().count("G") + seq.upper().count("C")) * 100
+    #gc_value = gc_count / len(seq)
+    #return gc_value
+    return "hello"
